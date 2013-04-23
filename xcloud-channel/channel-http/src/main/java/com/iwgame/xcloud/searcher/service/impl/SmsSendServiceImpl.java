98bc2bb70f44c041a17e009d5c8aadfbd3d62053
Wu Junjie <areskts@icloud.com>
2013-04-23 18:17:00 +0800

    Revert "httprequest"
    This reverts commit cd195eecb6a0ea2d00cccc1fdf989dd5ab84a727.

--- xcloud-channel/channel-http/src/main/java/com/iwgame/xcloud/searcher/service/impl/SmsSendServiceImpl.java
+++ xcloud-channel/channel-http/src/main/java/com/iwgame/xcloud/searcher/service/impl/SmsSendServiceImpl.java
@@ -1,16 +1,14 @@
 /****************************************************************
- * 文件名 ： SmsSendService.java
- * 日期 : 2012-9-7
- * Company: 上海绿岸网络科技有限公司
- * (C) Copyright Green Shore Network Technology Co.,Ltd.2012
- * All Rights Reserved.
- * 注意： 本内容仅限于上海绿岸网络科技有限公司内部使用，禁止转发
+ *  文件名     ： SmsSendService.java
+ *  日期         :  2012-9-7
+ *  Company: 上海绿岸网络科技有限公司
+ *  (C) Copyright Green Shore Network Technology Co.,Ltd.2012
+ *           		All Rights Reserved.
+ *  注意： 本内容仅限于上海绿岸网络科技有限公司内部使用，禁止转发
  ****************************************************************/
 package com.iwgame.xcloud.searcher.service.impl;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -26,13 +24,18 @@ import com.iwgame.xvalidators.Xvalidator;
  * 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
  * @类名:   SmsSendServiceImpl
+=======
+ * @类名:   SmsSendServiceImpl 
+>>>>>>> Revert "httprequest"
  * @描述:  	短信请求,发送到MQ队列相关实现类
  *
  * @作者:   吴君杰
  * @邮箱:	wujunjie@iwgame.com
  * @日期:	2012-10-24下午02:04:02
  * @版本:   1.0
+<<<<<<< HEAD
 =======
 =======
 >>>>>>> httprequest
@@ -47,6 +50,8 @@ import com.iwgame.xvalidators.Xvalidator;
 >>>>>>> httprequest
 =======
 >>>>>>> httprequest
+=======
+>>>>>>> Revert "httprequest"
  */
 @Service
 public class SmsSendServiceImpl implements SmsSendService {
@@ -55,7 +60,7 @@ public class SmsSendServiceImpl implements SmsSendService {
 
 	@Resource
 	private RabbitTemplate rabbitTemplateCommon;
-
+	
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -64,8 +69,9 @@ public class SmsSendServiceImpl implements SmsSendService {
 	 * .String, java.lang.String, com.iwgame.xcloud.searcher.model.SmsParamBean)
 	 */
 	@Override
-	public String sendSmsByMQ(final String pid, final SmsParamBean smsParamBean) {
+	public String sendSmsByMQ(String pid, SmsParamBean smsParamBean) {
 		logger.info("AppName:[" + smsParamBean.getAppname() + "]收到短信请求:" + smsParamBean.toString());
+<<<<<<< HEAD
 
 		final MQSendResult result = new MQSendResult();
 
@@ -77,11 +83,19 @@ public class SmsSendServiceImpl implements SmsSendService {
 >>>>>>> httprequest
 =======
 >>>>>>> httprequest
+=======
+		
+		MQSendResult result = new MQSendResult();	
+		
+		
+		
+>>>>>>> Revert "httprequest"
 		try {
 			// 验证必填参数
 			if (Xvalidator.getInstance().validate(smsParamBean)) {
-
+				
 				// 验证签名
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 				final int rc = SecurityUtil.securityAuthority(pid, smsParamBean.getPhone(), smsParamBean.getSign(), smsParamBean.getTs());
@@ -93,21 +107,23 @@ public class SmsSendServiceImpl implements SmsSendService {
 				final int rc = SecurityUtil.securityAuthority(pid, smsParamBean.getPhone(), smsParamBean.getSign(),
 						smsParamBean.getTs());
 >>>>>>> httprequest
+=======
+				int rc = SecurityUtil.securityAuthority(pid, smsParamBean.getPhone(), smsParamBean.getSign(), smsParamBean.getTs());
+>>>>>>> Revert "httprequest"
 				if (rc == 0) {
-					final String routeKey = "sms" + smsParamBean.getQueueNo();
+					String routeKey = "sms" + smsParamBean.getQueueNo();
 					rabbitTemplateCommon.convertAndSend("common.sms.exchange", routeKey, smsParamBean.toString());
-					logger.info("AppName:[" + smsParamBean.getAppname() + "]发送短信通道(task-common):(" + routeKey
-							+ ")成功!请求:" + smsParamBean.toString());
+					logger.info("AppName:[" + smsParamBean.getAppname() + "]发送短信通道(task-common):(" + routeKey + ")成功!请求:" + smsParamBean.toString());
 					result.setResult(rc);
-
+					
 				} else {
-					logger.error("AppName:[" + smsParamBean.getAppname() + "]签名验证失败!返回值[" + rc + "] 参数:[pid:" + pid
-							+ "] [Phone:" + smsParamBean.getPhone() + "] ");
+					logger.error("AppName:[" + smsParamBean.getAppname() + "]签名验证失败!返回值[" + rc + "] 参数:[pid:" + pid + "] [Phone:" + smsParamBean.getPhone() + "] ");
 					result.setResult(rc);
-
+					
 				}
 			} else {
 				result.setResult(-4);
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 				logger.error("AppName:[" + smsParamBean.getAppname() + "]请求参数错误,必要参数为空或NULL!请求:" + smsParamBean.toString());
@@ -120,14 +136,19 @@ public class SmsSendServiceImpl implements SmsSendService {
 						+ smsParamBean.toString());
 >>>>>>> httprequest
 
+=======
+				logger.error("AppName:[" + smsParamBean.getAppname() + "]请求参数错误,必要参数为空或NULL!请求:" + smsParamBean.toString());
+			
+>>>>>>> Revert "httprequest"
 			}
-		} catch (final Exception ex) {
+		} catch (Exception ex) {
 			result.setResult(-4);
 			logger.error("AppName:[" + smsParamBean.getAppname() + "]其他异常!请求:" + smsParamBean.toString(), ex);
-
+			
 		}
 		return result.toString();
 	}
+<<<<<<< HEAD
 
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -154,4 +175,6 @@ public class SmsSendServiceImpl implements SmsSendService {
 >>>>>>> httprequest
 		return request.getParameterMap().toString();
 	}
+=======
+>>>>>>> Revert "httprequest"
 }
