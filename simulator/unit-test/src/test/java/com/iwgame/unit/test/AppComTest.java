f5ef1c37b16c64c502de49e188f4816edd2e86ba
Wu Junjie <areskts@icloud.com>
2013-05-28 14:59:19 +0800

    test-unit

--- simulator/unit-test/src/test/java/com/iwgame/unit/test/AppComTest.java
+++ simulator/unit-test/src/test/java/com/iwgame/unit/test/AppComTest.java
@@ -183,294 +183,317 @@ public class AppComTest {
 		System.out.println(rs);
 	}
 
-	//
-	// /**
-	// * 密保卡测试
-	// */
-	// @TestConfig
-	// public void testSecurityCard() {
-	// WebResource client = Client.create().resource("");
-	//
-	// String pid = "P-P1";
-	//
-	// WebResource wr = client.path("http://data.iwgame.com/service/game/" + pid
-	// + "/sendsecuritycard");
-	//
-	// MultivaluedMap<String, String> param = new MultivaluedMapImpl();
-	//
-	// String ts = String.valueOf(System.currentTimeMillis());
-	//
-	// String username = "shumenceshi005";
-	// String guid = "dx1";
-	//
-	// String str = pid + "&" + username + "&" +
-	// "glWN8S1Al1JznVqjf1jV1CMOifQyp8Ve" + "&" + ts;
-	// String sign = MD5Util.md5sum(str);
-	//
-	// param.add("appname", "yunwei");
-	// param.add("ts", ts);
-	// param.add("sign", sign);
-	// param.add("guid", guid);
-	// param.add("name", username);
-	// param.add("type", "6");
-	// param.add("str3",
-	// "722337968335246445981686533794724395603395427965655695720745496692027295209220923293681771746988115637");
-	// param.add("operate", "2");
-	//
-	// String rs =
-	// wr.queryParams(param).accept(MediaType.APPLICATION_JSON).post(String.class);
-	//
-	// System.out.println(rs);
-	// }
-	//
-	// /**
-	// * 密保卡测试
-	// */
-	// @TestConfig
-	// public void testKickuser() {
-	// WebResource client = Client.create().resource("");
-	//
-	// String pid = "P-P1";
-	//
-	// WebResource wr = client.path("http://data.iwgame.com/service/game/" + pid
-	// + "/sendkickuser");
-	//
-	// MultivaluedMap<String, String> param = new MultivaluedMapImpl();
-	//
-	// String ts = String.valueOf(System.currentTimeMillis());
-	//
-	// String username = "shumenceshi005";
-	// String guid = "dx1";
-	//
-	// String str = pid + "&" + username + "&" +
-	// "glWN8S1Al1JznVqjf1jV1CMOifQyp8Ve" + "&" + ts;
-	// String sign = MD5Util.md5sum(str);
-	//
-	// param.add("appname", "yunwei");
-	// param.add("ts", ts);
-	// param.add("sign", sign);
-	// param.add("guid", guid);
-	// param.add("username", username);
-	//
-	// String rs =
-	// wr.queryParams(param).accept(MediaType.APPLICATION_JSON).post(String.class);
-	//
-	// System.out.println(rs);
-	// }
-	//
-	// @TestConfig
-	// public void testNoTalk() {
-	// WebResource client = Client.create().resource("");
-	//
-	// String pid = "P-P1";
-	//
-	// WebResource wr = client.path("http://data.iwgame.com/service/game/" + pid
-	// + "/sendnotalk");
-	//
-	// MultivaluedMap<String, String> param = new MultivaluedMapImpl();
-	//
-	// String ts = String.valueOf(System.currentTimeMillis());
-	//
-	// String username = "yunwei";
-	// String guid = "dx1";
-	//
-	// String str = pid + "&" + username + "&" +
-	// "glWN8S1Al1JznVqjf1jV1CMOifQyp8Ve" + "&" + ts;
-	// String sign = MD5Util.md5sum(str);
-	//
-	// param.add("appname", "yunwei");
-	// param.add("ts", ts);
-	// param.add("sign", sign);
-	// param.add("guid", guid);
-	// param.add("username", username);
-	// param.add("groupname", "电信二三区【新月风云】");
-	// param.add("validtime", "10");
-	//
-	// String rs =
-	// wr.queryParams(param).accept(MediaType.APPLICATION_JSON).post(String.class);
-	//
-	// System.out.println(rs);
-	// }
-	//
-	// @TestConfig
-	// public void testTalk() {
-	// WebResource client = Client.create().resource("");
-	//
-	// String pid = "P-P1";
-	//
-	// WebResource wr = client.path("http://data.iwgame.com/service/game/" + pid
-	// + "/sendtalk");
-	//
-	// MultivaluedMap<String, String> param = new MultivaluedMapImpl();
-	//
-	// String ts = String.valueOf(System.currentTimeMillis());
-	//
-	// String username = "yunwei";
-	// String guid = "dx1";
-	//
-	// String str = pid + "&" + username + "&" +
-	// "glWN8S1Al1JznVqjf1jV1CMOifQyp8Ve" + "&" + ts;
-	// String sign = MD5Util.md5sum(str);
-	//
-	// param.add("appname", "解除禁言");
-	// param.add("ts", ts);
-	// param.add("sign", sign);
-	// param.add("guid", guid);
-	// param.add("username", username);
-	// param.add("groupname", "电信二三区【新月风云】");
-	// param.add("validtime", "10");
-	//
-	// String rs =
-	// wr.queryParams(param).accept(MediaType.APPLICATION_JSON).post(String.class);
-	//
-	// System.out.println(rs);
-	// }
-	//
-	//
-	// /**
-	// * 账号解封
-	// */
-	// @Test
-	// public void testUnLockAccount() {
-	// WebResource client = Client.create().resource("");
-	//
-	// String pid = "P-P1";
-	//
-	// WebResource wr = client.path("http://data.iwgame.com/service/game/" + pid
-	// + "/sendunlockaccount");
-	//
-	// MultivaluedMap<String, String> param = new MultivaluedMapImpl();
-	//
-	// String ts = String.valueOf(System.currentTimeMillis());
-	//
-	// String username = "shumenceshi005";
-	// String guid = "dxfc";
-	//
-	// String str = pid + "&" + username + "&" +
-	// "glWN8S1Al1JznVqjf1jV1CMOifQyp8Ve" + "&" + ts;
-	// String sign = MD5Util.md5sum(str);
-	//
-	// param.add("appname", "unknown");
-	// param.add("ts", ts);
-	// param.add("sign", sign);
-	// param.add("guid", guid);
-	// param.add("username", username);
-	// param.add("sealtime", "10");
-	// param.add("rolename", "");
-	// param.add("note", "");
-	//
-	// String rs =
-	// wr.queryParams(param).accept(MediaType.APPLICATION_JSON).post(String.class);
-	//
-	// System.out.println(rs);
-	// }
+	/**
+	 * 密保卡测试
+	 */
+	@Test
+	public void testSecurityCard() {
+		final WebResource client = Client.create().resource("");
 
-	// /**
-	// * 账号解封
-	// */
-	// @Test
-	// public void testLockAccount() {
-	// WebResource client = Client.create().resource("");
-	//
-	// String pid = "P-P1";
-	//
-	// WebResource wr = client.path("http://data.iwgame.com/service/game/" + pid
-	// + "/sendlockaccount");
-	//
-	// MultivaluedMap<String, String> param = new MultivaluedMapImpl();
-	//
-	// String ts = String.valueOf(System.currentTimeMillis());
-	//
-	// String username = "shumenceshi005";
-	// String guid = "dxfc";
-	//
-	// String str = pid + "&" + username + "&" +
-	// "glWN8S1Al1JznVqjf1jV1CMOifQyp8Ve" + "&" + ts;
-	// String sign = MD5Util.md5sum(str);
-	//
-	// param.add("appname", "unknown");
-	// param.add("ts", ts);
-	// param.add("sign", sign);
-	// param.add("guid", guid);
-	// param.add("username", username);
-	// param.add("sealtime", "10");
-	// param.add("rolename", "");
-	// param.add("note", "");
-	//
-	// String rs =
-	// wr.queryParams(param).accept(MediaType.APPLICATION_JSON).post(String.class);
-	//
-	// System.out.println(rs);
-	// }
+		final String pid = "P-P1";
 
-	//
-	//
-	// @Test
-	// public void testActor() {
-	// WebResource client = Client.create().resource("");
-	//
-	// String pid = "P-P1";
-	//
-	// String accountid = "24667552";
-	//
-	// WebResource wr = client.path("http://data.iwgame.test/service/account/" +
-	// pid + "/" + accountid + "/getAccount");
-	//
-	// MultivaluedMap<String, String> param = new MultivaluedMapImpl();
-	//
-	// String ts = String.valueOf(System.currentTimeMillis());
-	//
-	// String str = pid + "&" + accountid + "&" +
-	// "glWN8S1Al1JznVqjf1jV1CMOifQyp8Ve" + "&" + ts;
-	// String sign = MD5Util.md5sum(str);
-	//
-	// param.add("appname", "获取角色信息测试");
-	// param.add("ts", ts);
-	// param.add("sign", sign);
-	//
-	// String rs =
-	// wr.queryParams(param).accept(MediaType.APPLICATION_JSON).get(String.class);
-	//
-	// System.out.println(rs);
-	// }
-	//
-	// @Test
-	// public void testGold() {
-	// WebResource client = Client.create().resource("");
-	// String pid = "P-P1";
-	//
-	// WebResource wr = client.path("http://data.iwgame.com/service/gold/" + pid
-	// + "/sendgold");
-	//
-	// MultivaluedMap<String, String> param = new MultivaluedMapImpl();
-	//
-	// String ts = String.valueOf(System.currentTimeMillis());
-	//
-	// String accountid = "335920";
-	//
-	// String str = pid + "&" + accountid + "&" +
-	// "glWN8S1Al1JznVqjf1jV1CMOifQyp8Ve" + "&" + ts;
-	// String sign = MD5Util.md5sum(str);
-	//
-	// param.add("appname", "wwwhd");
-	// param.add("ts", ts);
-	// param.add("sign", sign);
-	// param.add("activeid", "123456");
-	// param.add("activename", "送水晶test");
-	// param.add("accountid", accountid);
-	// param.add("name", "wujunjie");
-	//
-	// param.add("actor", "霸王别姬");
-	// param.add("type", "1");
-	// param.add("gameid", "2");
-	// param.add("zoneid", "2");
-	// param.add("value", "2");
-	//
-	// String rs =
-	// wr.queryParams(param).accept(MediaType.APPLICATION_JSON).post(String.class);
-	//
-	// System.out.println(rs);
-	// }
+		final WebResource wr = client.path("http://data.iwgame.com/service/game/" + pid + "/sendsecuritycard");
+
+		final MultivaluedMap<String, String> param = new MultivaluedMapImpl();
+
+		final String ts = String.valueOf(System.currentTimeMillis());
+
+		final String username = "shumenceshi005";
+		final String guid = "dx1";
+
+		final String str = pid + "&" + username + "&" + "glWN8S1Al1JznVqjf1jV1CMOifQyp8Ve" + "&" + ts;
+		final String sign = MD5Util.md5sum(str);
+
+		param.add("appname", "yunwei");
+		param.add("ts", ts);
+		param.add("sign", sign);
+		param.add("guid", guid);
+		param.add("name", username);
+		param.add("type", "6");
+		param.add("str3",
+				"722337968335246445981686533794724395603395427965655695720745496692027295209220923293681771746988115637");
+		param.add("operate", "2");
+
+		final String rs = wr.queryParams(param).accept(MediaType.APPLICATION_JSON).post(String.class);
+
+		System.out.println(rs);
+	}
+
+	/**
+	 * 密保卡测试
+	 */
+	@Test
+	public void testKickuser() {
+		final WebResource client = Client.create().resource("");
+
+		final String pid = "P-P1";
+
+		final WebResource wr = client.path("http://data.iwgame.com/service/game/" + pid + "/sendkickuser");
+
+		final MultivaluedMap<String, String> param = new MultivaluedMapImpl();
+
+		final String ts = String.valueOf(System.currentTimeMillis());
+
+		final String username = "shumenceshi005";
+		final String guid = "dx1";
+
+		final String str = pid + "&" + username + "&" + "glWN8S1Al1JznVqjf1jV1CMOifQyp8Ve" + "&" + ts;
+		final String sign = MD5Util.md5sum(str);
+
+		param.add("appname", "yunwei");
+		param.add("ts", ts);
+		param.add("sign", sign);
+		param.add("guid", guid);
+		param.add("username", username);
+
+		final String rs = wr.queryParams(param).accept(MediaType.APPLICATION_JSON).post(String.class);
+
+		System.out.println(rs);
+	}
+
+	@Test
+	public void testNoTalk() {
+		final WebResource client = Client.create().resource("");
+
+		final String pid = "P-P1";
+
+		final WebResource wr = client.path("http://data.iwgame.com/service/game/" + pid + "/sendnotalk");
+
+		final MultivaluedMap<String, String> param = new MultivaluedMapImpl();
+
+		final String ts = String.valueOf(System.currentTimeMillis());
+
+		final String username = "yunwei";
+		final String guid = "dx1";
+
+		final String str = pid + "&" + username + "&" + "glWN8S1Al1JznVqjf1jV1CMOifQyp8Ve" + "&" + ts;
+		final String sign = MD5Util.md5sum(str);
+
+		param.add("appname", "yunwei");
+		param.add("ts", ts);
+		param.add("sign", sign);
+		param.add("guid", guid);
+		param.add("username", username);
+		param.add("groupname", "电信二三区【新月风云】");
+		param.add("validtime", "10");
+
+		final String rs = wr.queryParams(param).accept(MediaType.APPLICATION_JSON).post(String.class);
+
+		System.out.println(rs);
+	}
+
+	@Test
+	public void testTalk() {
+		final WebResource client = Client.create().resource("");
+
+		final String pid = "P-P1";
+
+		final WebResource wr = client.path("http://data.iwgame.com/service/game/" + pid + "/sendtalk");
+
+		final MultivaluedMap<String, String> param = new MultivaluedMapImpl();
+
+		final String ts = String.valueOf(System.currentTimeMillis());
+
+		final String username = "yunwei";
+		final String guid = "dx1";
+
+		final String str = pid + "&" + username + "&" + "glWN8S1Al1JznVqjf1jV1CMOifQyp8Ve" + "&" + ts;
+		final String sign = MD5Util.md5sum(str);
+
+		param.add("appname", "解除禁言");
+		param.add("ts", ts);
+		param.add("sign", sign);
+		param.add("guid", guid);
+		param.add("username", username);
+		param.add("groupname", "电信二三区【新月风云】");
+		param.add("validtime", "10");
+
+		final String rs = wr.queryParams(param).accept(MediaType.APPLICATION_JSON).post(String.class);
+
+		System.out.println(rs);
+	}
+
+	/**
+	 * 账号解封
+	 */
+	@Test
+	public void testUnLockAccount() {
+		final WebResource client = Client.create().resource("");
+
+		final String pid = "P-P1";
+
+		final WebResource wr = client.path("http://data.iwgame.com/service/game/" + pid + "/sendunlockaccount");
+
+		final MultivaluedMap<String, String> param = new MultivaluedMapImpl();
+
+		final String ts = String.valueOf(System.currentTimeMillis());
+
+		final String username = "shumenceshi005";
+		final String guid = "dxfc";
+
+		final String str = pid + "&" + username + "&" + "glWN8S1Al1JznVqjf1jV1CMOifQyp8Ve" + "&" + ts;
+		final String sign = MD5Util.md5sum(str);
+
+		param.add("appname", "unknown");
+		param.add("ts", ts);
+		param.add("sign", sign);
+		param.add("guid", guid);
+		param.add("username", username);
+		param.add("sealtime", "10");
+		param.add("rolename", "");
+		param.add("note", "");
+
+		final String rs = wr.queryParams(param).accept(MediaType.APPLICATION_JSON).post(String.class);
+
+		System.out.println(rs);
+	}
+
+	/**
+	 * 账号解封
+	 */
+	@Test
+	public void testLockAccount() {
+		final WebResource client = Client.create().resource("");
+
+		final String pid = "P-P1";
+
+		final WebResource wr = client.path("http://data.iwgame.com/service/game/" + pid + "/sendlockaccount");
+
+		final MultivaluedMap<String, String> param = new MultivaluedMapImpl();
+
+		final String ts = String.valueOf(System.currentTimeMillis());
+
+		final String username = "shumenceshi005";
+		final String guid = "dxfc";
+
+		final String str = pid + "&" + username + "&" + "glWN8S1Al1JznVqjf1jV1CMOifQyp8Ve" + "&" + ts;
+		final String sign = MD5Util.md5sum(str);
+
+		param.add("appname", "unknown");
+		param.add("ts", ts);
+		param.add("sign", sign);
+		param.add("guid", guid);
+		param.add("username", username);
+		param.add("sealtime", "10");
+		param.add("rolename", "");
+		param.add("note", "");
+
+		final String rs = wr.queryParams(param).accept(MediaType.APPLICATION_JSON).post(String.class);
+
+		System.out.println(rs);
+	}
+
+	@Test
+	public void testActor() {
+		final WebResource client = Client.create().resource("");
+
+		final String pid = "P-P1";
+
+		final String accountid = "24667552";
+
+		final WebResource wr = client.path("http://data.iwgame.test/service/account/" + pid + "/" + accountid
+				+ "/getAccount");
+
+		final MultivaluedMap<String, String> param = new MultivaluedMapImpl();
+
+		final String ts = String.valueOf(System.currentTimeMillis());
+
+		final String str = pid + "&" + accountid + "&" + "glWN8S1Al1JznVqjf1jV1CMOifQyp8Ve" + "&" + ts;
+		final String sign = MD5Util.md5sum(str);
+
+		param.add("appname", "获取角色信息测试");
+		param.add("ts", ts);
+		param.add("sign", sign);
+
+		final String rs = wr.queryParams(param).accept(MediaType.APPLICATION_JSON).get(String.class);
+
+		System.out.println(rs);
+	}
+
+	@Test
+	public void testGold() {
+		final WebResource client = Client.create().resource("");
+		final String pid = "P-P1";
+
+		final WebResource wr = client.path("http://data.iwgame.com/service/gold/" + pid + "/sendgold");
+
+		final MultivaluedMap<String, String> param = new MultivaluedMapImpl();
+
+		final String ts = String.valueOf(System.currentTimeMillis());
+
+		final String accountid = "335920";
+
+		final String str = pid + "&" + accountid + "&" + "glWN8S1Al1JznVqjf1jV1CMOifQyp8Ve" + "&" + ts;
+		final String sign = MD5Util.md5sum(str);
+
+		param.add("appname", "wwwhd");
+		param.add("ts", ts);
+		param.add("sign", sign);
+		param.add("activeid", "123456");
+		param.add("activename", "送水晶test");
+		param.add("accountid", accountid);
+		param.add("name", "wujunjie");
+
+		param.add("actor", "霸王别姬");
+		param.add("type", "1");
+		param.add("gameid", "2");
+		param.add("zoneid", "2");
+		param.add("value", "2");
+
+		final String rs = wr.queryParams(param).accept(MediaType.APPLICATION_JSON).post(String.class);
+
+		System.out.println(rs);
+	}
+
+	@Test
+	public void test_IPTransfer() {
+		final WebResource client = Client.create().resource("");
+
+		final String pid = "common";
+
+		final String ip = "";
+
+		final WebResource wr = client.path("http://data.iwgame.com/service/iptranslate/"+pid+"/getTranslate");
+
+		final MultivaluedMap<String, String> param = new MultivaluedMapImpl();
+
+		final String ts = String.valueOf(System.currentTimeMillis());
+
+		final String str = pid + "&" + ip + "&" + "glWN8S1Al1JznVqjf1jV1CMOifQyp8Ve" + "&" + ts;
+		final String sign = MD5Util.md5sum(str);
+
+		param.add("appname", "security");
+		param.add("ts", ts);
+		param.add("sign", sign);
+		param.add("ip", ip);
+
+		final String rs = wr.queryParams(param).accept(MediaType.APPLICATION_JSON).get(String.class);
+		System.out.println(rs);
+	}
+
+	@Test
+	public void testItemCardActivity() {
+		final WebResource client = Client.create().resource("");
+		final String pid = "common";
+		final WebResource wr = client.path("http://hdapi.iwgame.com/service/sms/" + pid + "/sendsms");
+		final MultivaluedMap<String, String> param = new MultivaluedMapImpl();
+		final String ts = String.valueOf(System.currentTimeMillis());
+
+		final String phone = "13776801367";
+		final String str = pid + "&" + phone + "&" + "glWN8S1Al1JznVqjf1jV1CMOifQyp8Ve" + "&" + ts;
+		final String sign = MD5Util.md5sum(str);
+
+		param.add("appname", "sq");
+		param.add("ts", ts);
+		param.add("sign", sign);
+		param.add("phone", phone);
+		param.add("message", "这是测试短信");
+		param.add("queueNo", "3722");
+
+		final String rs = wr.queryParams(param).accept(MediaType.APPLICATION_JSON).post(String.class);
+		System.out.println(rs);
+	}
+
 
 	@Test
 	public void testCustomerService() {
@@ -501,30 +524,5 @@ public class AppComTest {
 		final String rs = wr.queryParams(param).accept(MediaType.APPLICATION_JSON).get(String.class);
 		System.out.println(rs);
 	}
-
-
-	@Test
-	public void testItemCardActivity() {
-		final WebResource client = Client.create().resource("");
-		final String pid = "common";
-		final WebResource wr = client.path("http://hdapi.iwgame.com/service/sms/" + pid + "/sendsms");
-		final MultivaluedMap<String, String> param = new MultivaluedMapImpl();
-		final String ts = String.valueOf(System.currentTimeMillis());
-
-		final String phone = "13776801367";
-		final String str = pid + "&" + phone + "&" + "glWN8S1Al1JznVqjf1jV1CMOifQyp8Ve" + "&" + ts;
-		final String sign = MD5Util.md5sum(str);
-
-		param.add("appname", "sq");
-		param.add("ts", ts);
-		param.add("sign", sign);
-		param.add("phone", phone);
-		param.add("message", "这是测试短信");
-		param.add("queueNo", "3722");
-
-		final String rs = wr.queryParams(param).accept(MediaType.APPLICATION_JSON).post(String.class);
-		System.out.println(rs);
-	}
-
 
 }
