ab8d57242f0d4069e14b0c2489ec961015b73947
Wu Junjie <areskts@icloud.com>
2013-05-28 15:00:28 +0800

    validator

--- xdeveloper/xdeveloper-xvalidator/src/main/java/jjwu/xdeveloper/xvalidators/Xvalidator.java
+++ xdeveloper/xdeveloper-xvalidator/src/main/java/jjwu/xdeveloper/xvalidators/Xvalidator.java
@@ -1,10 +1,10 @@
 /****************************************************************
- *  文件名     ：Xvalidator.java
- *  日期         :  2012-8-14
- *  Company: 上海绿岸网络科技有限公司
- *  (C) Copyright Green Shore Network Technology Co.,Ltd.2012
- *           		All Rights Reserved.
- *  注意： 本内容仅限于上海绿岸网络科技有限公司内部使用，禁止转发
+ * 文件名 ：Xvalidator.java
+ * 日期 : 2012-8-14
+ * Company: 上海绿岸网络科技有限公司
+ * (C) Copyright Green Shore Network Technology Co.,Ltd.2012
+ * All Rights Reserved.
+ * 注意： 本内容仅限于上海绿岸网络科技有限公司内部使用，禁止转发
  ****************************************************************/
 package jjwu.xdeveloper.xvalidators;
 
@@ -18,16 +18,15 @@ import jjwu.xdeveloper.xvalidators.exeception.XvalidatorException;
 import jjwu.xdeveloper.xvalidators.handlers.Handler;
 import jjwu.xdeveloper.xvalidators.util.GetFiledValue;
 
-
 /**
  * 
- * @类名:   Xvalidator
- * @描述:  	javaBean字段验证器,用于后台数据交验
- *
- * @作者:   吴君杰
- * @邮箱:	wujunjie@iwgame.com
- * @日期:	2012-10-25上午09:19:38
- * @版本:   1.0
+ * @类名: Xvalidator
+ * @描述: javaBean字段验证器,用于后台数据交验
+ * 
+ * @作者: 吴君杰
+ * @邮箱: wujunjie@iwgame.com
+ * @日期: 2012-10-25上午09:19:38
+ * @版本: 1.0
  */
 public final class Xvalidator {
 
@@ -101,8 +100,8 @@ public final class Xvalidator {
 			try {
 				destValue = GetFiledValue.getFieldValue(targetObj, field.getName());
 			} catch (final Exception ex) {
-				throw new XvalidatorException("Get field value or cast value error for field " + field.getName() + " in Class " + targetObj.getClass() + ". Error message: "
-						+ ex.getMessage(), ex);
+				throw new XvalidatorException("Get field value or cast value error for field " + field.getName()
+						+ " in Class " + targetObj.getClass() + ". Error message: " + ex.getMessage(), ex);
 			}
 			if (destValue == null) {
 				return; // NULL value is allowed.
