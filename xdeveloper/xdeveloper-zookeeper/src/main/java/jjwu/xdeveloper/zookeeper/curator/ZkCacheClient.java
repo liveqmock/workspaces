d10ef90a88d14582aaf12d2052bb84c298fb0358
wujunjie <areskts@icloud.com>
2013-12-05 17:23:40 +0800

    commit

--- xdeveloper/xdeveloper-zookeeper/src/main/java/jjwu/xdeveloper/zookeeper/curator/ZkCacheClient.java
+++ xdeveloper/xdeveloper-zookeeper/src/main/java/jjwu/xdeveloper/zookeeper/curator/ZkCacheClient.java
@@ -8,96 +8,161 @@ import java.util.concurrent.ConcurrentMap;
 
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.NodeCache;
+import org.apache.curator.framework.recipes.cache.NodeCacheListener;
+import org.apache.curator.framework.recipes.cache.PathChildrenCache;
+import org.apache.curator.framework.recipes.cache.PathChildrenCache.StartMode;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheListener;
 import org.apache.curator.framework.recipes.shared.SharedValue;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.data.Stat;
 import org.jboss.netty.util.internal.ConcurrentHashMap;
 
 import com.google.common.io.Closeables;
 
 public class ZkCacheClient {
+    
+    private static CuratorFramework                         client     = null;
+    
+    private final String                             zkhost;
+    
+    private final ConcurrentLinkedQueue<Closeable>   cacheList  = new ConcurrentLinkedQueue<Closeable>();
+    
+    private final ConcurrentMap<String, SharedValue> cacheValue = new ConcurrentHashMap<String, SharedValue>();
+    
+    public ZkCacheClient(String zkhost) {
 
-	private CuratorFramework client = null;
+        this.zkhost = zkhost;
+    }
+    
+    public void start() {
 
-	private final String zkhost;
+        try {
+            stop();
+            client = CuratorFrameworkFactory.newClient(zkhost, new ExponentialBackoffRetry(1000, 5));
+            client.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    public void stop() throws Exception {
 
-	private final static String nodePath = "/xcloud-sms-providers";
+        try {
+            for (Closeable closeable : cacheList) {
+                Closeables.close(closeable, false);
+            }
+            cacheList.clear();
+            
+            for (SharedValue sharedValue : cacheValue.values()) {
+                sharedValue.close();
+            }
+            cacheValue.clear();
+            
+            if (client != null) {
+                Closeables.close(client, false);
+                client = null;
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+    
+    public void setData(String nodePath, byte[] data) throws Exception {
 
-	private final ConcurrentLinkedQueue<Closeable> cacheList = new ConcurrentLinkedQueue<Closeable>();
+        SharedValue sharedValue = cacheValue.get(nodePath);
+        if (sharedValue == null) {
+            sharedValue = new SharedValue(client, nodePath, new String("").getBytes());
+            sharedValue.start();
+            cacheValue.put(nodePath, sharedValue);
+        }
+        sharedValue.setValue(data);
+    }
+    
+    public void deleteData(String nodePath) throws Exception {
 
-	private final ConcurrentMap<String, SharedValue> cacheValue = new ConcurrentHashMap<String, SharedValue>();
+        client.delete().forPath(nodePath);
+    }
+    
+    public byte[] getData(String nodePath) throws Exception {
 
-	public ZkCacheClient(String zkhost){
-		this.zkhost = zkhost;
-	}
+        SharedValue sharedValue = cacheValue.get(nodePath);
+        if (sharedValue == null) {
+            sharedValue = new SharedValue(client, nodePath, new String("").getBytes());
+            sharedValue.start();
+            cacheValue.put(nodePath, sharedValue);
+        } else {
+            return sharedValue.getValue();
+        }
+        return getData(nodePath);
+    }
+    
+    public Properties getNodeProperties(String nodePath) {
 
-	public void start(){
-		try {
-			stop();
-			client = CuratorFrameworkFactory.newClient(zkhost, new ExponentialBackoffRetry(1000, 5));
-			client.start();
-			getNodeProperties(ZkCacheClient.nodePath);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        try {
+            Properties props = new Properties();
+            byte[] datas = getData(nodePath);
+            if (null != datas) {
+                props.load(new ByteArrayInputStream(datas));
+            }
+            return props;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public void watcherPath(String nodePath) throws Throwable {
 
-	}
+        final PathChildrenCache cache = new PathChildrenCache(client, nodePath, false);
+        cache.getListenable().addListener(new PathChildrenCacheListener() {
+            
+            @Override
+            public void childEvent(CuratorFramework client, PathChildrenCacheEvent event) throws Exception {
 
-	public void stop() throws Exception{
-		try {
-			for (Closeable closeable : cacheList) {
-				Closeables.close(closeable, false);
-			}
-			cacheList.clear();
+                switch (event.getType()) {
+                    case CHILD_UPDATED:
+                        System.out.println("更新节点");
+                        break;
+                    case CHILD_REMOVED:
+                        System.out.println("删除节点");
+                        break;
+                    case CHILD_ADDED:
+                        System.out.println("添加节点");
+                        break;
+                    default:
+                        System.out.println("defautl");
+                        break;
+                }
+            }
+        });
+        cache.start(StartMode.BUILD_INITIAL_CACHE);
+    }
+    
+    public void watherNode(String nodePath) throws Throwable {
 
-			for (SharedValue sharedValue : cacheValue.values()) {
-				sharedValue.close();
-			}
-			cacheValue.clear();
-
-			if(client != null){
-				Closeables.close(client, false);
-				client = null;
-			}
-		} catch (Exception e) {
-			throw e;
-		}
-	}
-
-	public void setData(String nodePath,byte[] data) throws Exception{
-		SharedValue sharedValue = cacheValue.get(nodePath);
-		if(sharedValue == null){
-			sharedValue = new SharedValue(client, nodePath, new String("").getBytes());
-			sharedValue.start();
-			cacheValue.put(nodePath, sharedValue);
-		}
-		sharedValue.setValue(data);
-	}
-
-	public byte[] getData(String nodePath) throws Exception{
-		SharedValue sharedValue = cacheValue.get(nodePath);
-		if(sharedValue == null){
-			sharedValue = new SharedValue(client, nodePath, new String("").getBytes());
-			sharedValue.start();
-			cacheValue.put(nodePath, sharedValue);
-		}else{
-			return sharedValue.getValue();
-		}
-		return getData(nodePath);
-	}
-
-	public Properties getNodeProperties(String nodePath){
-
-		try {
-			Properties props = new Properties();
-			byte[] datas = getData(nodePath);
-			if(null != datas){
-				props.load(new ByteArrayInputStream(datas));
-			}
-			return props;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
+        final NodeCache cache = new NodeCache(client, nodePath);
+        cache.getListenable().addListener(new NodeCacheListener() {
+            
+            @Override
+            public void nodeChanged() throws Exception {
+                System.out.println("修改属性值");
+            }
+        });
+        cache.start(true);
+    }
+    
+    public void createPath(String path) throws Exception{
+        client.create().forPath(path);
+    }
+    
+    public Stat existsPath(String path) throws Exception{
+        return client.checkExists().forPath(path);
+    }
+    public void deletePath(String path) throws Exception{
+         client.delete().forPath(path);
+    }
+    
 }
--- xdeveloper/xdeveloper-zookeeper/src/test/java/jjwu/xdeveloper/zookeeper/AppTest.java
+++ xdeveloper/xdeveloper-zookeeper/src/test/java/jjwu/xdeveloper/zookeeper/AppTest.java
@@ -1,9 +1,5 @@
 package jjwu.xdeveloper.zookeeper;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 import jjwu.xdeveloper.zookeeper.curator.ZkCacheClient;
 import junit.framework.Assert;
 
@@ -13,51 +9,41 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class AppTest {
+    
+    private final static String nodePath = "/xcloud-sms-providers/hq";
+    private final static String nodeRoot = "/xcloud-sms-providers";
+    
+    @Test
+    public void testApp1() throws Throwable {
 
+        try {
+            ZkCacheClient client = new ZkCacheClient("127.0.0.1:2181");
+            client.start();
+            client.watcherPath(nodeRoot);
+            client.watherNode(nodePath);
+            while(true){
+                Thread.sleep(1000);
+            }
+           
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        Assert.assertTrue(true);
+    }
+    
+    @Test
+    public void testApp2() {
 
-	@Test
-	public void testApp() {
-
-		try {
-			ZooKeeperOperator zkoperator = new ZooKeeperOperator();
-			zkoperator.connect("127.0.0.1");
-			Map<String,String> data = new HashMap<String, String>();
-			data.put("username", "JJwu");
-			data.put("password", "wujunjie");
-
-			zkoperator.create("/root", null);
-
-			System.out.println(Arrays.toString(zkoperator.getData("/root")));
-
-			zkoperator.create("/root/child1", data.toString().getBytes());
-			System.out.println(Arrays.toString(zkoperator.getData("/root/child1")));
-
-			zkoperator.create("/root/child2", data.toString().getBytes());
-			System.out.println(Arrays.toString(zkoperator.getData("/root/child2")));
-
-			System.out.println("节点孩子信息:");
-			zkoperator.getChild("/root");
-
-			zkoperator.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		Assert.assertTrue(true);
-	}
-
-	@Test
-	public void testApp2() {
-
-		try {
-			ZkCacheClient client = new ZkCacheClient("/xcloud-sms");
-			client.start();
-
-			client.setData("queue/", "3733".getBytes());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		Assert.assertTrue(true);
-	}
+        try {
+            ZkCacheClient client = new ZkCacheClient("127.0.0.1:2181");
+            client.start();
+            client.deleteData(nodePath);
+            client.setData(nodePath, "love".getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        Assert.assertTrue(true);
+    }
 }
