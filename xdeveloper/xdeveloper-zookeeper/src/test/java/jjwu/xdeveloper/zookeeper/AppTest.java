2dead54411666a541888f3a0cdd68f15291c4aa5
wujunjie <areskts@icloud.com>
2013-12-22 19:57:16 +0800

    cm

--- xdeveloper/xdeveloper-zookeeper/src/test/java/jjwu/xdeveloper/zookeeper/AppTest.java
+++ xdeveloper/xdeveloper-zookeeper/src/test/java/jjwu/xdeveloper/zookeeper/AppTest.java
@@ -1,9 +1,5 @@
 package jjwu.xdeveloper.zookeeper;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 import jjwu.xdeveloper.zookeeper.curator.ZkCacheClient;
 import junit.framework.Assert;
 
@@ -13,51 +9,39 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class AppTest {
+    
+    private final static String nodePath = "/xcloud-sms-providers/hq";
+    
+    @Test
+    public void testApp1() throws Throwable {
 
+        try {
+            ZkCacheClient client = new ZkCacheClient("127.0.0.1:2181");
+            client.start();
+            client.watcherPath(nodePath);
+            client.watherNode(nodePath);
+            client.createPath(nodePath);
+            
+            client.setData(nodePath, "test".getBytes());
+           
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        Assert.assertTrue(true);
+    }
+    
+    @Test
+    public void testApp2() {
 
-	@Test
-	public void testApp() {
-
-		try {
-			ZooKeeperOperator zkoperator = new ZooKeeperOperator();
-			zkoperator.connect("127.0.0.1");
-			Map<String,String> data = new HashMap<String, String>();
-			data.put("username", "JJwu");
-			data.put("password", "wujunjie");
-
-			zkoperator.create("/root", null);
-
-			System.out.println(Arrays.toString(zkoperator.getData("/root")));
-
-			zkoperator.create("/root/child1", data.toString().getBytes());
-			System.out.println(Arrays.toString(zkoperator.getData("/root/child1")));
-
-			zkoperator.create("/root/child2", data.toString().getBytes());
-			System.out.println(Arrays.toString(zkoperator.getData("/root/child2")));
-
-			System.out.println("节点孩子信息:");
-			zkoperator.getChild("/root");
-
-			zkoperator.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		Assert.assertTrue(true);
-	}
-
-	@Test
-	public void testApp2() {
-
-		try {
-			ZkCacheClient client = new ZkCacheClient("/xcloud-sms");
-			client.start();
-
-			client.setData("queue/", "3733".getBytes());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		Assert.assertTrue(true);
-	}
+        try {
+            ZkCacheClient client = new ZkCacheClient("127.0.0.1:2181");
+            client.start();
+            client.deleteData(nodePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        Assert.assertTrue(true);
+    }
 }
